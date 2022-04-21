64bea7d44ef4cd3caff102593791d82cac48228b
hard0009 <armel.hardouin@etudiant.univ-reims.fr>
2022-04-21 16:41:26 +0200

    Ajout des rôles

---
+++ convenstage/resources/views/test/bar.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE=html>
+<html lang="fr">
+<head>
+    <title>bar</title>
+</head>
+<body>
+    <h1>bar</h1>
+</body>
+</html>
---
+++ convenstage/resources/views/test/foo.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE=html>
+<html lang="fr">
+<head>
+    <title>foo</title>
+</head>
+<body>
+    <h1>foo</h1>
+</body>
+</html>
